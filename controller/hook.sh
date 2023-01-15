65f00e366af13c1848b1b421e7b580b34194d981
Krzysztof Wiatrzyk <krzwiatrzyk@protonmail.com>
2023-01-15 15:03:54 +0100

    test

--- controller/hook.sh
+++ controller/hook.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 if [[ $1 == "--config" ]] ; then
-  cat trigger/pod_added.yaml
+  cat trigger/container_restarted.yaml
+  #cat trigger/pod_added.yaml
 else
   podName=$(jq -r .[0].object.metadata.name $BINDING_CONTEXT_PATH)
-  echo "Pod '${podName}' added"
+  if [[ ${podName} == "null" || ${podName} == "" ]]; then
+    exit 0
+  fi
+  apprise -t "Restart alert!" -b "Container in ${podName} was restarted" "gotifys://gotify.mirai.windkube.com/${GOTIFY_TOKEN}"
 fi
